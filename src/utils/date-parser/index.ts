b68ecd2739b2e1231d09f723072a062d52d2b2ca
Santosl2 <mfilype2019@gmail.com>
2025-03-22 12:40:33 -0300

    refactor: improve DateParser class with consistent formatting and additional method for end of day

--- src/utils/date-parser/index.ts
+++ src/utils/date-parser/index.ts
@@ -1,62 +1,67 @@
-import * as datefns from 'date-fns'
-import { ptBR } from 'date-fns/locale'
+import * as datefns from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 datefns.setDefaultOptions({
   locale: ptBR,
-})
+});
 
 // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 export class DateParser {
-  static fns = datefns
-  static lang = ptBR
+  static fns = datefns;
+  static lang = ptBR;
 
   static convertToMidnight(value: string | Date) {
-    if (value instanceof Date) return value
-    return new Date(`${value}T00:00:00`)
+    if (value instanceof Date) return value;
+    return new Date(`${value}T00:00:00`);
+  }
+
+  static convertToEndNight(value: string | Date) {
+    if (value instanceof Date) return value;
+    return new Date(`${value}T23:59:59`);
   }
 
   static formatToLocaleDateString(value: string | Date) {
     return DateParser.formatWithPattern(
       DateParser.convertToMidnight(value),
-      'dd/LL/yyyy'
-    )
+      "dd/LL/yyyy"
+    );
   }
 
   static getMonthName(value: string | Date) {
     return DateParser.formatWithPattern(
       DateParser.convertToMidnight(value),
-      'MMM'
-    )
+      "MMM"
+    );
   }
 
   static formatToLongDateString(date: Date) {
     try {
       return DateParser.fns.format(new Date(date), "dd 'de' MMM, yyy", {
         locale: ptBR,
-      })
+      });
     } catch {
-      return ''
+      return "";
     }
   }
 
-  static formatWithPattern(date: Date, formatString = 'yyyy-LL-dd') {
+  static formatWithPattern(date: Date, formatString = "yyyy-LL-dd") {
     try {
       return DateParser.fns.format(new Date(date), formatString, {
         locale: ptBR,
-      })
+      });
     } catch {
-      return ''
+      return "";
     }
   }
 
   static getPreviousDay() {
-    return DateParser.fns.subDays(new Date(), 1)
+    return DateParser.fns.subDays(new Date(), 1);
   }
 
   static getPreviousMonthName() {
     return DateParser.formatWithPattern(
       DateParser.fns.subMonths(new Date(), 1),
-      'LLL'
-    )
+      "LLL"
+    );
   }
 }
